a29b243bdb4acaa33717609c7cd913bb406c1e90
Jhonatan <jhow.mlourenco@gmail.com>
2026-04-03 22:43:38 -0300

    feat: Corrige erro no campo OBS em create cliente

--- src/modules/marmitas/dtos/create-clients/create-cliente.dto.ts
+++ src/modules/marmitas/dtos/create-clients/create-cliente.dto.ts
@@ -4,5 +4,5 @@ export const createClienteSchema = z.object({
   nome: z.string().min(1).max(255),
   endereco: z.string().min(1).max(255),
   telefone: z.string().min(10).max(12),
-  OBS: z.string().max(255).optional(),
+  obs: z.string().max(255).optional(),
 });
