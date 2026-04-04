cb4c50bd2d4b80ceddb6d54998a56ccd9add7b94
Jhonatan <jhow.mlourenco@gmail.com>
2026-04-03 22:26:59 -0300

    feat: adiciona rota de create clientes

--- src/modules/marmitas/marmita.routes.ts
+++ src/modules/marmitas/marmita.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { listMarmitasController } from './controllers/list-marmitas/list-marmitas.controller';
 import { createMarmitaController } from './controllers/create-marmita/create-marmita.controller';
+import { createClienteController } from './controllers/create-clients/create-clientes.controller';
 
 const router = Router();
 
 router.get('/', listMarmitasController);
 router.post('/', createMarmitaController);
+router.post('/', createClienteController)
 
 export default router;
